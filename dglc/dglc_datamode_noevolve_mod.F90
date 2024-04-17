c12a8d0c347e66e056c5f01f068fa563a05852ee
Mariana Vertenstein <mvertens@ucar.edu>
2024-04-17 11:04:49 -0600

    add Fogg_rofi to dglc

--- dglc/dglc_datamode_noevolve_mod.F90
+++ dglc/dglc_datamode_noevolve_mod.F90
@@ -39,6 +39,7 @@ module dglc_datamode_noevolve_mod
    type(icesheet_ptr_t), allocatable :: Sg_ice_covered(:)
    type(icesheet_ptr_t), allocatable :: Sg_icemask(:)
    type(icesheet_ptr_t), allocatable :: Sg_icemask_coupled_fluxes(:)
+   type(icesheet_ptr_t), allocatable :: Fogg_rofi(:)
 
    ! Import fields
    type(icesheet_ptr_t), allocatable :: Sl_tsrf(:)
@@ -50,6 +51,7 @@ module dglc_datamode_noevolve_mod
    character(len=*), parameter :: field_out_ice_covered            = 'Sg_ice_covered'
    character(len=*), parameter :: field_out_icemask                = 'Sg_icemask'
    character(len=*), parameter :: field_out_icemask_coupled_fluxes = 'Sg_icemask_coupled_fluxes'
+   character(len=*), parameter :: field_out_rofi                   = 'Fogg_rofi'
 
    ! Import Field names
    character(len=*), parameter :: field_in_tsrf                    = 'Sl_tsrf'
@@ -96,6 +98,7 @@ contains
       call dshr_fldList_add(fldsExport, field_out_topo)
       call dshr_fldList_add(fldsExport, field_out_icemask)
       call dshr_fldList_add(fldsExport, field_out_icemask_coupled_fluxes)
+      call dshr_fldList_add(fldsExport, field_out_rofi)
 
       do ns = 1,num_icesheets
         write(cnum,'(i0)') ns
@@ -150,6 +153,7 @@ contains
       allocate(Sg_ice_covered(num_icesheets))
       allocate(Sg_icemask(num_icesheets))
       allocate(Sg_icemask_coupled_fluxes(num_icesheets))
+      allocate(Fogg_rofi(num_icesheets))
 
       do ns = 1,num_icesheets
          call dshr_state_getfldptr(NStateExp(ns), field_out_area, fldptr1=Sg_area(ns)%ptr, rc=rc)
@@ -162,6 +166,8 @@ contains
          if (chkerr(rc,__LINE__,u_FILE_u)) return
          call dshr_state_getfldptr(NStateExp(ns), field_out_icemask_coupled_fluxes, fldptr1=Sg_icemask_coupled_fluxes(ns)%ptr, rc=rc)
          if (chkerr(rc,__LINE__,u_FILE_u)) return
+         call dshr_state_getfldptr(NStateExp(ns), field_out_rofi, fldptr1=Fogg_rofi(ns)%ptr, rc=rc)
+         if (chkerr(rc,__LINE__,u_FILE_u)) return
       end do
 
       ! initialize pointers to import fields if appropriate
@@ -169,16 +175,15 @@ contains
       allocate(Flgl_qice(num_icesheets))
 
       do ns = 1,num_icesheets
-         ! NOTE: the field is connected ONLY if the MED->GLC entry is in the nuopc.runconfig file
-         ! This restriction occurs even if the field was advertised
-         if (NUOPC_IsConnected(NStateImp(ns), fieldName=field_in_tsrf)) then
-            call dshr_state_getfldptr(NStateImp(ns), field_in_tsrf, fldptr1=Sl_tsrf(ns)%ptr, rc=rc)
-            if (chkerr(rc,__LINE__,u_FILE_u)) return
+         if (.not. NUOPC_IsConnected(NStateImp(ns), fieldName=field_in_tsrf)) then
+            ! NOTE: the field is connected ONLY if the MED->GLC entry is in the nuopc.runconfig file
+            ! This restriction occurs even if the field was advertised
+            call shr_sys_abort(trim(subname)//": MED->GLC must appear in run sequence")
          end if
-         if (NUOPC_IsConnected(NStateImp(ns), fieldName=field_in_qice)) then
-            call dshr_state_getfldptr(NStateImp(ns), field_in_qice, fldptr1=Flgl_qice(ns)%ptr, rc=rc)
-            if (chkerr(rc,__LINE__,u_FILE_u)) return
-         end if
+         call dshr_state_getfldptr(NStateImp(ns), field_in_tsrf, fldptr1=Sl_tsrf(ns)%ptr, rc=rc)
+         if (chkerr(rc,__LINE__,u_FILE_u)) return
+         call dshr_state_getfldptr(NStateImp(ns), field_in_qice, fldptr1=Flgl_qice(ns)%ptr, rc=rc)
+         if (chkerr(rc,__LINE__,u_FILE_u)) return
       end do
 
    end subroutine dglc_datamode_noevolve_init_pointers
@@ -226,124 +231,140 @@ contains
 
       rc = ESMF_SUCCESS
 
-      if (initialized_noevolve) then
-         RETURN
+      if (.not. initialized_noevolve) then
+
+         do ns = 1,num_icesheets
+
+            ! Get mesh info
+            call ESMF_MeshGet(model_meshes(ns), elementdistGrid=distGrid, rc=rc)
+            if (ChkErr(rc,__LINE__,u_FILE_u)) return
+            call ESMF_DistGridGet(distGrid, localDe=0, elementCount=lsize, rc=rc)
+            if (ChkErr(rc,__LINE__,u_FILE_u)) return
+            allocate(gindex(lsize))
+            call ESMF_DistGridGet(distGrid, localDe=0, seqIndexList=gindex, rc=rc)
+            if (ChkErr(rc,__LINE__,u_FILE_u)) return
+
+            ! Determine "glc_area" ;
+            ! Sg_areas is in radians
+            ! SHR_CONST_REARTH is the radius of earth in m
+            ! model_internal_gridsize is the internal model gridsize in m
+            do ng = 1,lsize
+               Sg_area(ns)%ptr(ng) = (model_internal_gridsize(ns)/SHR_CONST_REARTH)**2
+            end do
+
+            ! Create module level field bundle
+            fldbun_noevolve = ESMF_FieldBundleCreate(rc=rc) ! input field bundle
+
+            ! "ice thickness" ;
+            field_noevolve = ESMF_FieldCreate(model_meshes(ns), ESMF_TYPEKIND_R8, name='thk', meshloc=ESMF_MESHLOC_ELEMENT, rc=rc)
+            if (chkerr(rc,__LINE__,u_FILE_u)) return
+            call ESMF_FieldBundleAdd(fldbun_noevolve, (/field_noevolve/), rc=rc)
+            if (chkerr(rc,__LINE__,u_FILE_u)) return
+
+            ! "bed topography" ;
+            field_noevolve = ESMF_FieldCreate(model_meshes(ns), ESMF_TYPEKIND_R8, name='topg', meshloc=ESMF_MESHLOC_ELEMENT, rc=rc)
+            if (chkerr(rc,__LINE__,u_FILE_u)) return
+            call ESMF_FieldBundleAdd(fldbun_noevolve, (/field_noevolve/), rc=rc)
+            if (chkerr(rc,__LINE__,u_FILE_u)) return
+
+            ! Create pioid and pio_iodesc at the module level
+            inquire(file=trim(model_datafiles(ns)), exist=exists)
+            if (.not.exists) then
+               write(6,'(a)')' ERROR: model input file '//trim(model_datafiles(ns))//' does not exist'
+               call shr_sys_abort()
+            end if
+            rcode = pio_openfile(pio_subsystem, pioid, io_type, trim(model_datafiles(ns)), pio_nowrite)
+            call pio_seterrorhandling(pioid, PIO_BCAST_ERROR)
+            rcode = pio_inq_varid(pioid, 'thk', varid)
+            rcode = pio_inq_varndims(pioid, varid, ndims)
+            allocate(dimid(ndims))
+            rcode = pio_inq_vardimid(pioid, varid, dimid(1:ndims))
+            rcode = pio_inq_dimlen(pioid, dimid(1), nxg)
+            rcode = pio_inq_dimlen(pioid, dimid(2), nyg)
+            call pio_initdecomp(pio_subsystem, pio_double, (/nxg,nyg/), gindex, pio_iodesc)
+            deallocate(dimid)
+            deallocate(gindex)
+
+            ! Read in the data into the appropriate field bundle pointers
+            ! Note that Sg_ice_covered(ns)%ptr points into the data for
+            ! the Sg_ice_covered field in NStateExp(ns)
+            ! Note that Sg_topo(ns)%ptr points into the data for
+            ! the Sg_topon NStateExp(ns)
+            ! Note that topog is bedrock topography
+
+            call dshr_fldbun_getFldPtr(fldbun_noevolve, 'topg', topog, rc=rc)
+            if (ChkErr(rc,__LINE__,u_FILE_u)) return
+            rcode = pio_inq_varid(pioid, 'topg', varid)
+            call pio_read_darray(pioid, varid, pio_iodesc, topog, rcode)
+
+            call dshr_fldbun_getFldPtr(fldbun_noevolve, 'thk', thck, rc=rc)
+            if (ChkErr(rc,__LINE__,u_FILE_u)) return
+            rcode = pio_inq_varid(pioid, 'thk', varid)
+            call pio_read_darray(pioid, varid, pio_iodesc, thck,  rcode)
+
+            allocate(lsrf(lsize))
+            allocate(usrf(lsize))
+
+            rhoi = SHR_CONST_RHOICE   ! 0.917e3
+            rhoo = SHR_CONST_RHOSW    ! 1.026e3
+            eus = 0
+            do ng = 1,lsize
+               if (topog(ng) - eus < (-rhoi/rhoo) * thck(ng)) then
+                  lsrf(ng) = (-rhoi/rhoo) * thck(ng)
+               else
+                  lsrf(ng) = topog(ng)
+               end if
+               usrf(ng) = max(0.d0, thck(ng) + lsrf(ng))
+
+               ! The export field 'ice_mask_coupled_fluxes' determines who is handling the
+               ! runoff associated with the surface mass balance
+               ! If its 0 -then ctsm needs to handle it.
+               ! Since we want dglc to handle it no evolve mode - then
+               ! ice_mask_coupled_fluxes to be identical to the mask
+
+               if (is_in_active_grid(usrf(ng))) then
+                  Sg_icemask(ns)%ptr(ng) = 1.d0
+                  Sg_icemask_coupled_fluxes(ns)%ptr(ng) = 1.d0
+                  if (is_ice_covered(thck(ng))) then
+                     Sg_ice_covered(ns)%ptr(ng) = 1.d0
+                  else
+                     Sg_ice_covered(ns)%ptr(ng) = 0.d0
+                  end if
+                  ! Note that we use the same method for computing topo whether this point is
+                  ! ice-covered or ice-free. This is in contrast to the method for computing
+                  ! ice-free topo in glint_upscaling_gcm.
+                  Sg_topo(ns)%ptr(ng) = thk0 * usrf(ng)
+               else
+                  ! Note that this logic implies that if (in theory) we had an ice-covered
+                  ! point outside the "active grid", it will get classified as ice-free for
+                  ! these purposes. This mimics the logic currently in glint_upscaling_gcm.
+                  Sg_icemask(ns)%ptr(ng) = 0.d0
+                  Sg_icemask_coupled_fluxes(ns)%ptr(ng) = 0.d0
+                  Sg_ice_covered(ns)%ptr(ng) = 0.d0
+                  Sg_topo(ns)%ptr(ng) = 0.d0
+               end if
+            end do
+
+            deallocate(lsrf)
+            deallocate(usrf)
+
+            call pio_closefile(pioid)
+            call pio_freedecomp(pio_subsystem, pio_iodesc)
+
+         end do ! end loop over ice sheets
+
       end if
 
-      do ns = 1,num_icesheets
+      if (initialized_noevolve) then
+         ! Compute Fogg_rofi
+         do ns = 1,num_icesheets
+            do ng = 1,size(Fogg_rofi(ns)%ptr)
+               Fogg_rofi(ns)%ptr(ng) = Flgl_qice(ns)%ptr(ng)
+            end do
+         end do
+      end if
 
-        ! Get mesh info
-        call ESMF_MeshGet(model_meshes(ns), elementdistGrid=distGrid, rc=rc)
-        if (ChkErr(rc,__LINE__,u_FILE_u)) return
-        call ESMF_DistGridGet(distGrid, localDe=0, elementCount=lsize, rc=rc)
-        if (ChkErr(rc,__LINE__,u_FILE_u)) return
-        allocate(gindex(lsize))
-        call ESMF_DistGridGet(distGrid, localDe=0, seqIndexList=gindex, rc=rc)
-        if (ChkErr(rc,__LINE__,u_FILE_u)) return
-
-        ! Determine "glc_area" ;
-        ! Sg_areas is in radians
-        ! SHR_CONST_REARTH is the radius of earth in m
-        ! model_internal_gridsize is the internal model gridsize in m
-        do ng = 1,lsize
-           Sg_area(ns)%ptr(ng) = (model_internal_gridsize(ns)/SHR_CONST_REARTH)**2
-        end do
-
-        ! Create module level field bundle
-        fldbun_noevolve = ESMF_FieldBundleCreate(rc=rc) ! input field bundle
-
-        ! "ice thickness" ;
-        field_noevolve = ESMF_FieldCreate(model_meshes(ns), ESMF_TYPEKIND_R8, name='thk', meshloc=ESMF_MESHLOC_ELEMENT, rc=rc)
-        if (chkerr(rc,__LINE__,u_FILE_u)) return
-        call ESMF_FieldBundleAdd(fldbun_noevolve, (/field_noevolve/), rc=rc)
-        if (chkerr(rc,__LINE__,u_FILE_u)) return
-
-        ! "bed topography" ;
-        field_noevolve = ESMF_FieldCreate(model_meshes(ns), ESMF_TYPEKIND_R8, name='topg', meshloc=ESMF_MESHLOC_ELEMENT, rc=rc)
-        if (chkerr(rc,__LINE__,u_FILE_u)) return
-        call ESMF_FieldBundleAdd(fldbun_noevolve, (/field_noevolve/), rc=rc)
-        if (chkerr(rc,__LINE__,u_FILE_u)) return
-
-        ! Create pioid and pio_iodesc at the module level
-        inquire(file=trim(model_datafiles(ns)), exist=exists)
-        if (.not.exists) then
-           write(6,'(a)')' ERROR: model input file '//trim(model_datafiles(ns))//' does not exist'
-           call shr_sys_abort()
-        end if
-        rcode = pio_openfile(pio_subsystem, pioid, io_type, trim(model_datafiles(ns)), pio_nowrite)
-        call pio_seterrorhandling(pioid, PIO_BCAST_ERROR)
-        rcode = pio_inq_varid(pioid, 'thk', varid)
-        rcode = pio_inq_varndims(pioid, varid, ndims)
-        allocate(dimid(ndims))
-        rcode = pio_inq_vardimid(pioid, varid, dimid(1:ndims))
-        rcode = pio_inq_dimlen(pioid, dimid(1), nxg)
-        rcode = pio_inq_dimlen(pioid, dimid(2), nyg)
-        call pio_initdecomp(pio_subsystem, pio_double, (/nxg,nyg/), gindex, pio_iodesc)
-        deallocate(dimid)
-        deallocate(gindex)
-
-        ! Read in the data into the appropriate field bundle pointers
-        ! Note that Sg_ice_covered(ns)%ptr points into the data for
-        ! the Sg_ice_covered field in NStateExp(ns)
-        ! Note that Sg_topo(ns)%ptr points into the data for
-        ! the Sg_topon NStateExp(ns)
-        ! Note that topog is bedrock topography
-
-        call dshr_fldbun_getFldPtr(fldbun_noevolve, 'topg', topog, rc=rc)
-        if (ChkErr(rc,__LINE__,u_FILE_u)) return
-        rcode = pio_inq_varid(pioid, 'topg', varid)
-        call pio_read_darray(pioid, varid, pio_iodesc, topog, rcode)
-
-        call dshr_fldbun_getFldPtr(fldbun_noevolve, 'thk', thck, rc=rc)
-        if (ChkErr(rc,__LINE__,u_FILE_u)) return
-        rcode = pio_inq_varid(pioid, 'thk', varid)
-        call pio_read_darray(pioid, varid, pio_iodesc, thck,  rcode)
-
-        allocate(lsrf(lsize))
-        allocate(usrf(lsize))
-
-        rhoi = SHR_CONST_RHOICE   ! 0.917e3
-        rhoo = SHR_CONST_RHOSW    ! 1.026e3
-        eus = 0
-        do ng = 1,lsize
-           if (topog(ng) - eus < (-rhoi/rhoo) * thck(ng)) then
-              lsrf(ng) = (-rhoi/rhoo) * thck(ng)
-           else
-              lsrf(ng) = topog(ng)
-           end if
-           usrf(ng) = max(0.d0, thck(ng) + lsrf(ng))
-
-           if (is_in_active_grid(usrf(ng))) then
-              Sg_icemask(ns)%ptr(ng) = 1.d0
-              Sg_icemask_coupled_fluxes(ns)%ptr(ng) = 0.d0
-              if (is_ice_covered(thck(ng))) then
-                 Sg_ice_covered(ns)%ptr(ng) = 1.d0
-              else
-                 Sg_ice_covered(ns)%ptr(ng) = 0.d0
-              end if
-              ! Note that we use the same method for computing topo whether this point is
-              ! ice-covered or ice-free. This is in contrast to the method for computing
-              ! ice-free topo in glint_upscaling_gcm.
-              Sg_topo(ns)%ptr(ng) = thk0 * usrf(ng)
-           else
-              ! Note that this logic implies that if (in theory) we had an ice-covered
-              ! point outside the "active grid", it will get classified as ice-free for
-              ! these purposes. This mimics the logic currently in glint_upscaling_gcm.
-              Sg_icemask(ns)%ptr(ng) = 0.d0
-              Sg_icemask_coupled_fluxes(ns)%ptr(ng) = 0.d0
-              Sg_ice_covered(ns)%ptr(ng) = 0.d0
-              Sg_topo(ns)%ptr(ng) = 0.d0
-           end if
-        end do
-
-        deallocate(lsrf)
-        deallocate(usrf)
-
-        call pio_closefile(pioid)
-        call pio_freedecomp(pio_subsystem, pio_iodesc)
-
-      end do ! end loop over ice sheets
-
+      ! Set initialized flag
       initialized_noevolve = .true.
 
    end subroutine dglc_datamode_noevolve_advance
