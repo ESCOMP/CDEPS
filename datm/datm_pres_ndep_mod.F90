8ef224a79e3e9785e94b6413ce36e7e7b423c920
Mariana Vertenstein <marianav@met.no>
2026-01-06 12:38:38 +0100

    new cmip7 default settings for datm ndep forcing

--- datm/datm_pres_ndep_mod.F90
+++ datm/datm_pres_ndep_mod.F90
@@ -18,13 +18,13 @@ module datm_pres_ndep_mod
   real(r8), pointer :: Faxa_ndep(:,:) => null()
 
   ! stream data
-  real(r8), pointer :: strm_ndep_nhx_dry(:) => null() ! stream cmip7 ndep data
-  real(r8), pointer :: strm_ndep_nhx_wet(:) => null() ! stream cmip7 ndep data
-  real(r8), pointer :: strm_ndep_noy_dry(:) => null() ! stream cmip7 ndep data
-  real(r8), pointer :: strm_ndep_noy_wet(:) => null() ! stream cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_nhx_dry(:) => null() ! stream cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_nhx_wet(:) => null() ! stream cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_noy_dry(:) => null() ! stream cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_noy_wet(:) => null() ! stream cmip7 ndep data
 
-  real(r8), pointer :: strm_ndep_nhx(:)     => null() ! pre-cmip7 ndep data
-  real(r8), pointer :: strm_ndep_noy(:)     => null() ! pre-cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_nhx(:)     => null() ! pre-cmip7 ndep data
+  real(r8), pointer :: strm_Faxa_ndep_noy(:)     => null() ! pre-cmip7 ndep data
 
   logical :: use_cmip7_ndep
 
@@ -66,27 +66,27 @@ contains
     ! Get pointer to stream data that will be used below - if the
     ! following stream fields are not in any sdat streams, then a null value is returned
 
-    ! cmip7 forcing
-    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_nhx_dry', strm_ndep_nhx_dry, rc)
+    ! cmip7 ndep forcing
+    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_nhx_dry', strm_Faxa_ndep_nhx_dry, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
-    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_nhx_wet', strm_ndep_nhx_wet, rc)
+    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_nhx_wet', strm_Faxa_ndep_nhx_wet, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
-    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_noy_dry', strm_ndep_noy_dry, rc)
+    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_noy_dry', strm_Faxa_ndep_noy_dry, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
-    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_noy_wet', strm_ndep_noy_wet, rc)
+    call shr_strdata_get_stream_pointer(sdat, 'Faxa_ndep_noy_wet', strm_Faxa_ndep_noy_wet, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
-    ! older ndep forcing
-    call shr_strdata_get_stream_pointer( sdat, 'Faxa_ndep_nhx', strm_ndep_nhx, rc)
+    ! cmip6 ndep forcing
+    call shr_strdata_get_stream_pointer( sdat, 'Faxa_ndep_nhx', strm_Faxa_ndep_nhx, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
-    call shr_strdata_get_stream_pointer( sdat, 'Faxa_ndep_noy', strm_ndep_noy, rc)
+    call shr_strdata_get_stream_pointer( sdat, 'Faxa_ndep_noy', strm_Faxa_ndep_noy, rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
     ! determine use_cmip_ndep module variable
-    if (associated(strm_ndep_nhx_dry) .and. associated(strm_ndep_nhx_wet) .and. &
-        associated(strm_ndep_noy_dry) .and. associated(strm_ndep_noy_wet)) then
+    if (associated(strm_Faxa_ndep_nhx_dry) .and. associated(strm_Faxa_ndep_nhx_wet) .and. &
+        associated(strm_Faxa_ndep_noy_dry) .and. associated(strm_Faxa_ndep_noy_wet)) then
        use_cmip7_ndep = .true.
-    else if (associated(strm_ndep_nhx) .and. associated(strm_ndep_noy)) then
+    else if (associated(strm_Faxa_ndep_nhx) .and. associated(strm_Faxa_ndep_noy)) then
        use_cmip7_ndep = .false.
     else
        call shr_log_error('datm_ndep_advance: ERROR: no associated stream pointers for ndep forcing', rc=rc)
@@ -100,12 +100,12 @@ contains
 
     if (use_cmip7_ndep) then
        ! assume data is in kgN/m2/s
-       Faxa_ndep(1,:) = strm_ndep_nhx_dry(:) + strm_ndep_nhx_wet(:)
-       Faxa_ndep(2,:) = strm_ndep_noy_dry(:) + strm_ndep_noy_wet(:)
+       Faxa_ndep(1,:) = strm_Faxa_ndep_nhx_dry(:) + strm_Faxa_ndep_nhx_wet(:)
+       Faxa_ndep(2,:) = strm_Faxa_ndep_noy_dry(:) + strm_Faxa_ndep_noy_wet(:)
     else
        ! convert ndep flux to units of kgN/m2/s (input is in gN/m2/s)
-       Faxa_ndep(1,:) = strm_ndep_nhx(:) / 1000._r8
-       Faxa_ndep(2,:) = strm_ndep_noy(:) / 1000._r8
+       Faxa_ndep(1,:) = strm_Faxa_ndep_nhx(:) / 1000._r8
+       Faxa_ndep(2,:) = strm_Faxa_ndep_noy(:) / 1000._r8
     end if
 
   end subroutine datm_pres_ndep_advance
